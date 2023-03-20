83762817b9f8aa9c344bd180f00044ab6331836c
MuratVasiliev <vasiliymuratov04@gmail.com>
2023-03-21 01:24:50 +0300

    added_task 3

--- Lab_1_subfiles/L1T3.cpp
+++ Lab_1_subfiles/L1T3.cpp
@@ -15,32 +15,18 @@ int GetRandomNumber(int N)
 	int num = dstr(rng);
     return num;
 }
-
-
-bool binpoisk(int* mas, int N, int a){
-    int midd = 0;
-    int right=N-1;
-    int left=0;
-	while (1)
-	{
-		midd = (left + right) / 2;
-		
-		if (a < mas[midd]) 
-			right = midd - 1;      
-		else if (a > mas[midd])  
-			left = midd + 1;	   
-		else                       
-			return true;           
-
-		if (left > right)          
-			return false;
-	}
-    }
-
+bool sumpoisk(int* mas, int N, int a){
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+        if (mas[i]+mas[j]==a){
+            return true;
+        }
+    }}
+}
 
 int main(){
-    int h=60000;
-    int shag=500;
+    int h=1000;
+    int shag=5000;
     int ite=1;
     int MAX=100000;
     int k = (ite)*(MAX-100)/shag;
@@ -56,7 +42,7 @@ int main(){
     long long count=0;
     for (k=0; k<h; k++){
     auto start = chrono::steady_clock::now();
-    binpoisk(arr, N, GetRandomNumber(N));
+    sumpoisk(arr, N, -1);
     auto stop = chrono::steady_clock::now();
     auto time_span = chrono::duration_cast < chrono::nanoseconds> (stop - start);
     count=count+time_span.count();
