2aa6d1a7ba518662e6d398d2edc98e01082ffd81
MuratVasiliev <vasiliymuratov04@gmail.com>
2023-03-23 15:06:30 +0300

    added_G3

--- Lab_1_subfiles/L1T3.cpp
+++ Lab_1_subfiles/L1T3.cpp
@@ -15,17 +15,27 @@ int GetRandomNumber(int N)
 	int num = dstr(rng);
     return num;
 }
-bool sumpoisk(int* mas, int N, int a){
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-        if (mas[i]+mas[j]==a){
-            return true;
-        }
-    }}
+bool sumpoisk(int* mas, int N, int sum)
+    {
+    int l = 0, r = N-1;
+    
+    while (l!= r)
+    {
+        int cur_sum = mas[l] + mas[r];
+        
+        if (cur_sum > sum) r--;
+        
+        else if (cur_sum < sum) l++;
+        
+        else return true;
+    }
+    
+    return false;
 }
 
+
 int main(){
-    int h=100;
+    int h=5000;
     int shag=1000;
     int ite=1;
     int MAX=100000;
@@ -42,7 +52,7 @@ int main(){
     long long count=0;
     for (k=0; k<h; k++){
     auto start = chrono::steady_clock::now();
-    sumpoisk(arr, N, -1);
+    sumpoisk(arr, N, GetRandomNumber(2*N));
     auto stop = chrono::steady_clock::now();
     auto time_span = chrono::duration_cast < chrono::nanoseconds> (stop - start);
     count=count+time_span.count();
